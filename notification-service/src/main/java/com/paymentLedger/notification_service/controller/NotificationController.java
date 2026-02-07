93748a361ffac47206d28c89538d424444d245c0
GoodnesSheCodes <goodness.she.codes@gmail.com>
2026-02-07 23:04:35 +0200

    controller

--- notification-service/src/main/java/com/paymentLedger/notification_service/controller/NotificationController.java
+++ notification-service/src/main/java/com/paymentLedger/notification_service/controller/NotificationController.java
@@ -1,6 +1,27 @@
 package com.paymentLedger.notification_service.controler;
 
+import com.paypal.notification_service.entity.Notification;
+import com.paypal.notification_service.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notify")
 public class NotificationController {
+
+      @Autowired
+    private NotificationService notificationService;
+
+    @PostMapping
+    public Notification sendNotification(@RequestBody Notification notification) {
+        return notificationService.sendNotification(notification);
+    }
+
+    @GetMapping("/{userId}")
+    public List<Notification> getNotificationsByUser(@PathVariable Long userId) {
+        return notificationService.getNotificationsByUserId(userId);
+    }
 
 }
